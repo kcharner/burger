21efe3e4426eeb11a8b4d5f25d9f91485884bfaf
Krista Charner <kristacharner@outlook.com>
2017-03-22 10:42:59 -0400

    initial commit: cloned repo, created db folder w/schema and seeds sql files, generated server.js file, package.json file updated w/ required npm packages, added a gitignore

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+Create a burgers table with these fields:
+id: an auto incrementing int that serves as the primary key.
+burger_name: a string.
+devoured: a boolean.
+date: a TIMESTAMP.
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean,
+	burgerDate TIMESTAMP,
+	PRIMARY KEY (id)
+);
