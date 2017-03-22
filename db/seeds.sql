e4c1ee098de9f1dce31557ac6ef8b8735f552ba4
Krista Charner <kristacharner@outlook.com>
2017-03-22 10:58:26 -0400

    added insert queries for updating burgers table, successfully sourced sql files into db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, burgerDate) VALUES ("Grand Mac", 1, "2017-01-23 12:30:07");
+INSERT INTO burgers (burger_name, devoured, burgerDate) VALUES ("Baconator", 1, "2017-03-07 6:10:53");
+INSERT INTO burgers (burger_name, devoured, burgerDate) VALUES ("Whopper", 1, "2017-03-20 7:22:13");
